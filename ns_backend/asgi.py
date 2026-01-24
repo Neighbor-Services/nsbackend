5340e174d28ed747cbca5d28b595ed6d2b1432f8
afarisamuel <afari@thinkce.org>
2026-01-24 02:51:29 +0000

    first commit

--- ns_backend/asgi.py
+++ ns_backend/asgi.py
@@ -9,16 +9,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ns_backend.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry is populated
+# before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 import notifications.routing
 import tracking.routing
 from ns_backend.middleware import JWTAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ns_backend.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
