f1dec1728a16767f8ac975e7adbecb62360a2bcf
afarisamuel <afari@thinkce.org>
2026-04-24 14:27:49 +0000

    update

--- services/views.py
+++ services/views.py
@@ -293,6 +293,7 @@ class ProposalViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if not serializer.is_valid():
+            print(f"Proposal creation errors: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
